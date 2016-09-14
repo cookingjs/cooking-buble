b9a462f9cee7f2da1271682596295c04a2d17714
qingwei.li <cinwell.li@gmail.com>
2016-09-14 08:39:02 +0800

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# cooking-buble
+> A cooking plugin.
+
+# include
+
+
+# License
+MIT
---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @param  {object} cooking - add, remove, _userConfig and config
+ * @param  {*} options - custom option
+ */
+module.exports = function (cooking, options) {
+  cooking.add('loader.js', {
+    test: /\.(jsx?|babel|es6)$/,
+    include: process.cwd(),
+    exclude: /node_modules|bower_components/,
+    loaders: ['buble-loader']
+  })
+};

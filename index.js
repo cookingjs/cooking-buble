3f99c1f6865bc78372a3d316070c886ea8e3f2cd
qingwei.li <cinwell.li@gmail.com>
2016-09-16 16:26:58 +0800

    check webpack version

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var webpack = require('webpack')
+var isNextWebpack = false
+try {
+  isNextWebpack = require('cooking/util/check').isNextWebpack
+} catch(_) {}
 
 /**
  * @param  {object} cooking - add, remove, _userConfig and config
  * @param  {*} options - custom option
  */
 module.exports = function (cooking, options) {
+  if (!isNextWebpack) throw Error('[cooking-buble] need webpack 2');
+
   var config = cooking.config.module.loaders.js
   var test = config.test
-
-  cooking.add('loader.js', {
+  var loader = {
     test: test,
     include: config.include,
     exclude: config.exclude,
@@ -16,9 +21,12 @@ module.exports = function (cooking, options) {
     query: {
       objectAssign: 'Object.assign'
     }
-  })
+  }
+
 
   if (!!require('vue-loader')) {
-    cooking.add('vue.loaders.js', 'buble-loader?objectAssign=Object.assign')
+    cooking.add('vue.loaders.js', 'buble-loader?objectAssign=Object.assign&jsx=h')
+    loader.query.jsx='h'
   }
+  cooking.add('loader.js', loader)
 }
